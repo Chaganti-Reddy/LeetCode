73977ff97f1971e22b73b7723a90cd3c2ac3f65e
Chaganti Reddy <97330178+Chaganti-Reddy@users.noreply.github.com>
2025-04-05 15:37:55 +0530

    Time: 0 ms (100%), Space: 18.4 MB (25.33%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,22 +1,20 @@
+from typing import List
+
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        def binary_search(low, end, row):
-            if low > end:
+        def binary_search(low, high, row):
+            if low > high:
                 return False
-            mid = (low + end) // 2
+            mid = (low + high) // 2
             if matrix[row][mid] == target:
                 return True
             elif matrix[row][mid] > target:
-                return binary_search(low, mid-1, row)
+                return binary_search(low, mid - 1, row)
             else:
-                return binary_search(mid + 1, end, row)
+                return binary_search(mid + 1, high, row)
 
         for row in range(len(matrix)):
-            if matrix[row][-1] < target:
-                continue
-            elif matrix[row][0] == target or matrix[row][-1] == target:
-                return True
-            else:
-                return binary_search(0, len(matrix[row]), row)
-            
+            if matrix[row][0] <= target <= matrix[row][-1]:
+                if binary_search(0, len(matrix[row]) - 1, row):
+                    return True
         return False
