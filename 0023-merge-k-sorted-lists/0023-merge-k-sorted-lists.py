1b31ba907e18b8e8ab97a65d675aede6fcaa8eee
Chaganti Reddy <97330178+Chaganti-Reddy@users.noreply.github.com>
2025-04-12 14:47:43 +0530

    Time: 16 ms (33.54%), Space: 20 MB (97.65%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -3,16 +3,41 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
-    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        l = []
-        for list in lists:
-            while list:
-                l.append(list.val)
-                list = list.next
+    def mergeKLists(self, lists):
+        if not lists or len(lists) == 0:
+            return None
+        return self.divide(lists, 0, len(lists) - 1)
+
+    def divide(self, lists, l, r):
+        if l > r:
+            return None
+        if l == r:
+            return lists[l]
+
+        mid = l + (r - l) // 2
+        left = self.divide(lists, l, mid)
+        right = self.divide(lists, mid + 1, r)
+
+        return self.conquer(left, right)
+
+    def conquer(self, l1, l2):
         dummy = ListNode(0)
         curr = dummy
-        for val in sorted(l):
-            dummy.next = ListNode(val)
-            dummy = dummy.next
-        return curr.next
+
+        while l1 and l2:
+            if l1.val <= l2.val:
+                curr.next = l1
+                l1 = l1.next
+            else:
+                curr.next = l2
+                l2 = l2.next
+            curr = curr.next
+
+        if l1:
+            curr.next = l1
+        else:
+            curr.next = l2
+
+        return dummy.next
