59cd4f03ad41e16e1b62e36f5a68652bfceddfd3
Chaganti Reddy <97330178+Chaganti-Reddy@users.noreply.github.com>
2025-04-19 20:59:31 +0530

    Time: 227 ms (57.42%), Space: 31.5 MB (53.88%) - LeetHub

--- 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.py
+++ 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.py
@@ -3,15 +3,12 @@ from bisect import bisect_left, bisect_right
 class Solution:
     def countFairPairs(self, nums: List[int], lower: int, upper: int) -> int:
         nums.sort()
-
-        def count_common(l, u):
-            return bisect_right(nums, u) - bisect_left(nums, l)
-
+        n = len(nums)
         ans = 0
-        for num in nums:
-            if (upper - num >= num and lower - num <= num):
-                ans += (count_common(lower-num, upper-num) - 1)
-            else:
-                ans += count_common(lower-num, upper-num)
 
-        return ans//2
+        for i in range(n):
+            l = bisect_left(nums, lower - nums[i], i + 1, n)
+            r = bisect_right(nums, upper - nums[i], i + 1, n)
+            ans += (r - l)
+
+        return ans
