37173556ea3140bc928482a03e1091cc0b1a6996
Chaganti Reddy <97330178+Chaganti-Reddy@users.noreply.github.com>
2025-04-20 13:51:18 +0530

    Time: 0 ms (100%), Space: 17.9 MB (76.07%) - LeetHub

--- 0797-rabbits-in-forest/0797-rabbits-in-forest.py
+++ 0797-rabbits-in-forest/0797-rabbits-in-forest.py
@@ -1,14 +1,8 @@
 class Solution:
     def numRabbits(self, answers: List[int]) -> int:
-        answers.sort()
-        ans = count = 0
-        for i, v in enumerate(answers):
-            if v == 0:
-                ans += 1
-            elif (i == 0 or v != answers[i-1] or count == 0):
-                ans += v + 1
-                count = v
-            else:
-                count -= 1
+        m = Counter(answers)
+        ans = 0
+        for i in m:
+            ans += ceil(float(m[i])/(i+1))*(i + 1)
 
-        return ans
+        return ans 
