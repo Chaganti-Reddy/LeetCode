24ceba52d7f14cf0bcdfceb9c638a173cad4c504
Chaganti Reddy <97330178+Chaganti-Reddy@users.noreply.github.com>
2026-01-14 17:05:43 +0530

    0883-CAR-FLEET Stats: Time: 315 ms (8.11%), Space: 41 MB (30.66%) - LeetHub

--- 0883-car-fleet/0883-car-fleet.py
+++ 0883-car-fleet/0883-car-fleet.py
@@ -4,6 +4,7 @@ class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
         stack = []
         for p, s in sorted(zip(position, speed), reverse=True):
+            print(p, s)
             time = (target - p) / s
             if stack and time <= stack[-1]:
                 continue  
