665ef8aafb696d692bcc6a45e59d0571a399adde
Chaganti Reddy <97330178+Chaganti-Reddy@users.noreply.github.com>
2026-04-02 15:14:41 +0530

    3677-MAXIMUM-AMOUNT-OF-MONEY-ROBOT-CAN-EARN Stats: Time: 10 ms (75%), Space: 4.6 MB (100%) - LeetHub

--- 3677-maximum-amount-of-money-robot-can-earn/3677-maximum-amount-of-money-robot-can-earn.rs
+++ 3677-maximum-amount-of-money-robot-can-earn/3677-maximum-amount-of-money-robot-can-earn.rs
@@ -2,44 +2,38 @@ impl Solution {
     pub fn maximum_amount(coins: Vec<Vec<i32>>) -> i32 {
         let m = coins.len();
         let n = coins[0].len();
-        let neg_inf = -1e15 as i64;
-        let mut prev_row = vec![vec![neg_inf; 3]; n];
+        const NEG_INF: i64 = -1e15 as i64;
+        let mut dp = vec![[NEG_INF; 3]; n];
 
         for i in 0..m {
-            let mut curr_row = vec![vec![neg_inf; 3]; n];
-            
             for j in 0..n {
                 let val = coins[i][j] as i64;
+                let mut current = [NEG_INF; 3];
 
                 if i == 0 && j == 0 {
-                    curr_row[0][0] = val;
-                    if val < 0 { curr_row[0][1] = 0; }
-                } else {
+                    current[0] = val;
+                    current[1] = if val < 0 { 0 } else { val };
+                    current[2] = if val < 0 { 0 } else { val };
+                } else {                    
                     for k in 0..3 {
-                        let mut prev_same_k = neg_inf;
-                        if i > 0 { prev_same_k = prev_same_k.max(prev_row[j][k]); }
-                        if j > 0 { prev_same_k = prev_same_k.max(curr_row[j-1][k]); }
-                        
-                        if prev_same_k != neg_inf {
-                            curr_row[j][k] = curr_row[j][k].max(prev_same_k + val);
-                        }
+                        let mut best_prev = NEG_INF;
+                        if i > 0 { best_prev = best_prev.max(dp[j][k]); }
+                        if j > 0 { best_prev = best_prev.max(dp[j-1][k]); }
+
+                        current[k] = best_prev + val;
 
                         if val < 0 && k > 0 {
-                            let mut prev_less_k = neg_inf;
-                            if i > 0 { prev_less_k = prev_less_k.max(prev_row[j][k-1]); }
-                            if j > 0 { prev_less_k = prev_less_k.max(curr_row[j-1][k-1]); }
-                            
-                            if prev_less_k != neg_inf {
-                                curr_row[j][k] = curr_row[j][k].max(prev_less_k);
-                            }
+                            let mut best_prev_for_skip = NEG_INF;
+                            if i > 0 { best_prev_for_skip = best_prev_for_skip.max(dp[j][k-1]); }
+                            if j > 0 { best_prev_for_skip = best_prev_for_skip.max(dp[j-1][k-1]); }
+                            current[k] = current[k].max(best_prev_for_skip);
                         }
                     }
                 }
+                dp[j] = current;
             }
-            prev_row = curr_row;
         }
 
-        let res = prev_row[n-1][0].max(prev_row[n-1][1]).max(prev_row[n-1][2]);
-        res as i32
+        dp[n - 1].iter().max().copied().unwrap() as i32
     }
 }
